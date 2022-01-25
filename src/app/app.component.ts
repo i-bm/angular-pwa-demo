771c59baec2d44e174a5a3e3e4954bf451dfca67
i-bm <isaac.boakyemanu@gmail.com>
2022-01-25 09:30:17 +0000

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './service/post.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-pwa-demo';
+  postData: any;
+
+  constructor(private postService: PostService) { }
+  ngOnInit(): void {
+    this.onGetPosts();
+  }
+
+  onGetPosts(): void {
+    // this.postService.getPosts().subscribe(
+    //   (response) => console.log(response),
+    //   (error: any) => console.log(error),
+    //   () => console.log('Done getting posts!')
+    // );
+    let result = this.postService.getPosts()
+    result.subscribe((data) => this.postData = data)
+  }
 }
